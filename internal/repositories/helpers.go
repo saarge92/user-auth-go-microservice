039dc76c3bc0f8ca5893b8a26ad91ef7a08d58cd
Serdar Durdyev <durdyev.s@nakukop.com>
2021-08-28 21:00:41 +0400

    First commit

--- internal/repositories/helpers.go
+++ internal/repositories/helpers.go
@@ -1,8 +1,12 @@
 package repositories
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
 func lastInsertID(result sql.Result) int32 {
 	id, _ := result.LastInsertId()
+	fmt.Println(id)
 	return int32(id)
 }
