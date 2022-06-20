4d15762cc4c00e3132a2b0902e74af5be98f711f
Serdar <durdyev.s@nakukop.com>
2022-06-20 15:19:30 +0400

    Add sign in roles response

--- internal/app/user/transformer.go
+++ internal/app/user/transformer.go
@@ -6,13 +6,6 @@ import (
 )
 
 func FromUserResponseToGRPC(userResponse *dto.UserRole, token string) *core.SignInResponse {
-	// var grpcRoles = make([]*core.Role, 0, len(userResponse.Roles))
-	// for _, role := range userResponse.Roles {
-	//	grpcRoles = append(grpcRoles, &core.Role{
-	//		Id:   role.ID,
-	//		Name: role.Name,
-	//	})
-	//}
 	return &core.SignInResponse{
 		Id:    userResponse.User.ID,
 		Token: token,
